7ee07573a051ba2f6edf5e493849c09e00300aa2
layegithub <99507927+layegithub@users.noreply.github.com>
2022-03-14 17:08:34 +0000

    Add files via upload

---
+++ Inscription.js
@@ -0,0 +1,6 @@
+var size = document.getElementsByName("nom");
+console.log(size.maxLength = 25);
+var size = document.getElementsByName("prenom");
+console.log(size.maxLength = 25);
+var size = document.getElementsByName("numero");
+console.log(size.maxLength = 9);
